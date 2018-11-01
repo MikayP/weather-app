490ad85057b70b165a7f85fdd4974c27b49635f8
Mike Portman <mikeportmanj@gmail.com>
2018-10-31 23:17:47 -0700

    initial commit

---
+++ src/components/titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const titles = () => (
+            <div>
+                <h1 className="title-container__title">Weather Finder</h1>
+                <p className="title-container__subtitle">Find out tempoerature, condition, and more...</p>
+            </div>
+)
+
+export default titles;
